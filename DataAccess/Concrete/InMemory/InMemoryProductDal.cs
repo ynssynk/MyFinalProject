909c334c7a7ed40beb6f0d2aa6975ccc2bd378e7
ynssynk <ynssynk@hotmail.com>
2021-02-06 23:56:01 +0300

    6 şubat dto eklendi

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Concrete.InMemory
 {
@@ -57,6 +58,11 @@ namespace DataAccess.Concrete.InMemory
             Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
             
             _products.Remove(productToDelete);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
